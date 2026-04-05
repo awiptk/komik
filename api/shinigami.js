d134d99d0d1dcfdc388a750120c30367b5018bc1
awiptk <awiselow123@gmail.com>
2026-04-06 03:35:36 +0700

    Update shinigami.js

--- api/shinigami.js
+++ api/shinigami.js
@@ -1,6 +1,6 @@
 import { SHINIGAMI } from './_utils.js';
 
-export const runtime = 'edge';
+export const config = { runtime: 'edge' };
 
 export default async function handler(req) {
   const { searchParams } = new URL(req.url);
